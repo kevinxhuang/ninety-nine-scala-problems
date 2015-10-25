9aab7d68c1e17695ef2684ecc918f5f1577ef0cf
Kevin Huang <killua_hzl@163.com>
2015-10-25 20:08:31 +0800

    Solution for P36 to P41.

--- src/main/scala/Arithmetic.scala
+++ src/main/scala/Arithmetic.scala
@@ -69,4 +69,81 @@ object Arithmetic {
       return getFactors(n, (2 to n).filter(isPrime).toStream)
     }
   }
+
+  //  P36 (**) Determine the prime factors of a given positive integer (2).
+  //    Construct a list containing the prime factors and their multiplicity.
+  //  scala> 315.primeFactorMultiplicity
+  //  res0: List[(Int, Int)] = List((3,2), (5,1), (7,1))
+  //  Alternately, use a Map for the result.
+  //
+  //  scala> 315.primeFactorMultiplicity
+  //  res0: Map[Int,Int] = Map(3 -> 2, 5 -> 1, 7 -> 1)
+  def primeFactorMultiplicity(n: Int): Map[Int, Int] = {
+    var factorCount = Map[Int, Int]()
+    primeFactors(n).foreach(f => factorCount += f -> (factorCount.getOrElse(f, 0) + 1))
+
+    return factorCount
+  }
+
+  //  P37 (**) Calculate Euler's totient function phi(m) (improved).
+  //    See problem P34 for the definition of Euler's totient function. If the list of the prime factors of a number m is known in the form of problem P36 then the function phi(m>) can be efficiently calculated as follows: Let [[p1, m1], [p2, m2], [p3, m3], ...] be the list of prime factors (and their multiplicities) of a given number m. Then phi(m) can be calculated with the following formula:
+  //    phi(m) = (p1-1)*p1(m1-1) * (p2-1)*p2(m2-1) * (p3-1)*p3(m3-1) * ...
+  //
+  //  Note that ab stands for the bth power of a.
+  def phi(m: Int): Int = {
+    return primeFactorMultiplicity(m).foldLeft(1)({(x , y) => x * (y._1 - 1) * Math.pow(y._1, y._2 - 1).toInt})
+  }
+
+  //  P39 (*) A list of prime numbers.
+  //  Given a range of integers by its lower and upper limit, construct a list of all prime numbers in that range.
+  //  scala> listPrimesinRange(7 to 31)
+  //  res0: List[Int] = List(7, 11, 13, 17, 19, 23, 29, 31)
+  def listPrimesinRange(range: Range): List[Int] = {
+    return range.toList.filter(isPrime _)
+  }
+
+  //  P40 (**) Goldbach's conjecture.
+  //  Goldbach's conjecture says that every positive even number greater than 2 is the sum of two prime numbers. E.g. 28 = 5 + 23. It is one of the most famous facts in number theory that has not been proved to be correct in the general case. It has been numerically confirmed up to very large numbers (much larger than Scala's Int can represent). Write a function to find the two prime numbers that sum up to a given even integer.
+  //    scala> 28.goldbach
+  //  res0: (Int, Int) = (5,23)
+  def goldbach(n: Int): (Int, Int) = {
+    val p = listPrimesinRange(2 to n).find(p => isPrime(n - p))
+    return (p.get, n - p.get)
+  }
+
+  //  P41 (**) A list of Goldbach compositions.
+  //  Given a range of integers by its lower and upper limit, print a list of all even numbers and their Goldbach composition.
+  //    scala> printGoldbachList(9 to 20)
+  //  10 = 3 + 7
+  //  12 = 5 + 7
+  //  14 = 3 + 11
+  //  16 = 3 + 13
+  //  18 = 5 + 13
+  //  20 = 3 + 17
+  //  In most cases, if an even number is written as the sum of two prime numbers, one of them is very small. Very rarely, the primes are both bigger than, say, 50. Try to find out how many such cases there are in the range 2..3000.
+  //
+  //  Example (minimum value of 50 for the primes):
+  //
+  //  scala> printGoldbachListLimited(1 to 2000, 50)
+  //  992 = 73 + 919
+  //  1382 = 61 + 1321
+  //  1856 = 67 + 1789
+  //  1928 = 61 + 1867
+  def printGoldbachListGoldbachList(range: Range): Unit = {
+    range.foreach(x => {
+      if (x % 2 == 0) {
+        val g = goldbach(x)
+        Console.out.println(x + " = " + g._1 + " + " + g._2)
+      }
+    })
+  }
+
+  def printGoldbachListLimited(range: Range, limit: Int): Unit = {
+    range.filter(_ > limit).foreach(x => {
+      if (x % 2 == 0) {
+        val g = goldbach(x)
+        Console.out.println(x + " = " + g._1 + " + " + g._2)
+      }
+    })
+  }
 }
--- src/test/scala/ArithmeticTest.scala
+++ src/test/scala/ArithmeticTest.scala
@@ -15,41 +15,46 @@ class ArithmeticTest {
     assertEquals(Arithmetic.gcd(36, 63), 9)
   }
 
+  @Test
   def testP33(): Unit = {
-    assertEquals(Arithmetic.gcd(35, 64), true)
-    assertEquals(Arithmetic.gcd(36, 64), false)
+    assertEquals(Arithmetic.isCoprimeTo(35, 64), true)
+    assertEquals(Arithmetic.isCoprimeTo(36, 64), false)
   }
 
+  @Test
   def testP34(): Unit = {
     assertEquals(Arithmetic.totient(10), 4)
   }
 
+  @Test
   def testP35(): Unit = {
     assertEquals(Arithmetic.primeFactors(2), List(2))
     assertEquals(Arithmetic.primeFactors(315), List(3, 3, 5, 7))
   }
 
+  @Test
   def testP36(): Unit = {
-
+    assertEquals(Arithmetic.primeFactorMultiplicity(315), Map(3 -> 2, 5 -> 1, 7 -> 1))
   }
 
+  @Test
   def testP37(): Unit = {
-
+    assertEquals(Arithmetic.phi(315), 144)
   }
 
-  def testP38(): Unit = {
-
-  }
-
+  @Test
   def testP39(): Unit = {
-
+    assertEquals(Arithmetic.listPrimesinRange(7 to 31), List(7, 11, 13, 17, 19, 23, 29, 31))
   }
 
+  @Test
   def testP40(): Unit = {
-
+    assertEquals(Arithmetic.goldbach(28), (5, 23))
   }
 
+  @Test
   def testP41(): Unit = {
-
+    Arithmetic.printGoldbachListGoldbachList(9 to 20)
+    Arithmetic.printGoldbachListLimited(1 to 2000, 50)
   }
 }
